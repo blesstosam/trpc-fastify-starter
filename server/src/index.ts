b85c5d626634c69e02edd9fb87aa3b8486dca31f
blesstosam <805902285@qq.com>
2026-02-12 10:58:56 +0800

    feat: app分离

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,65 +1,11 @@
-import scalarApi from '@scalar/fastify-api-reference'
-import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify'
-import Fastify, { FastifyInstance } from 'fastify'
-import { generateOpenApiDocument } from 'trpc-to-openapi'
-import { getJwtSecret } from './lib/auth'
-import { createContext } from './rpc/context'
-import { appRouter } from './rpc/router'
+import { buildApp } from './app'
 
-async function main() {
-  const app = Fastify({
-    logger: true,
-  })
+const app = buildApp()
 
-  await app.register(import('@fastify/cors'), {
-    origin: true,
-  })
-
-  await app.register(import('@fastify/jwt'), {
-    secret: getJwtSecret(),
-  })
-
-  await app.register(fastifyTRPCPlugin, {
-    prefix: '/rpc',
-    trpcOptions: {
-      router: appRouter,
-      createContext,
-    },
-  })
-
-  await initDoc(app)
-
-  await app.listen({
-    host: '0.0.0.0',
-    port: 2022,
-  })
-}
-
-async function initDoc(app: FastifyInstance) {
-  const openApiDocument = generateOpenApiDocument(appRouter, {
-    title: 'tRPC OpenAPI',
-    description: 'API docs for this tRPC project.',
-    version: '1.0.0',
-    baseUrl: 'http://localhost:2022/rpc',
-    securitySchemes: {
-      bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        description: 'Bearer token auth.',
-      },
-    },
-  })
-
-  await app.register(scalarApi, {
-    routePrefix: '/doc',
-    configuration: {
-      content: openApiDocument,
-    },
-  })
-}
-
-main().catch((err) => {
+app.listen({
+  host: '0.0.0.0',
+  port: 2022,
+}).catch((err) => {
   console.error(err)
   process.exit(1)
 })
